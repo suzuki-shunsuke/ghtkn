999344836a88e894dbabb98d4c04daa493d58689
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-08-31 14:12:59 +0900

    style: add code comment

--- pkg/keyring/mock.go
+++ pkg/keyring/mock.go
@@ -4,20 +4,28 @@ import (
 	"github.com/zalando/go-keyring"
 )
 
+// mockAPI is a mock implementation of the API interface for testing.
+// It stores secrets in memory instead of using the system keyring.
 type mockAPI struct {
 	secrets map[string]string
 }
 
+// NewMockAPI creates a new mock API instance with the provided initial secrets.
+// If secrets is nil, an empty map will be created when needed.
 func NewMockAPI(secrets map[string]string) API {
 	return &mockAPI{
 		secrets: secrets,
 	}
 }
 
+// mockKey generates a unique key for storing secrets by combining service and user.
+// The format is "service:user".
 func mockKey(service, user string) string {
 	return service + ":" + user
 }
 
+// Get retrieves a secret from the mock keyring.
+// Returns keyring.ErrNotFound if the secret doesn't exist.
 func (m *mockAPI) Get(service, user string) (string, error) {
 	k := mockKey(service, user)
 	s, ok := m.secrets[k]
@@ -27,6 +35,8 @@ func (m *mockAPI) Get(service, user string) (string, error) {
 	return s, nil
 }
 
+// Set stores a secret in the mock keyring.
+// Creates the internal map if it doesn't exist.
 func (m *mockAPI) Set(service, user, password string) error {
 	if m.secrets == nil {
 		m.secrets = make(map[string]string)
@@ -35,6 +45,8 @@ func (m *mockAPI) Set(service, user, password string) error {
 	return nil
 }
 
+// Delete removes a secret from the mock keyring.
+// No error is returned if the secret doesn't exist.
 func (m *mockAPI) Delete(service, user string) error {
 	k := mockKey(service, user)
 	delete(m.secrets, k)
