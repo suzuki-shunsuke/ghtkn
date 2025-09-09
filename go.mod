1e1c68fedcfb33029be5db7a3e7b30520d336166
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-09-09 22:51:33 +0900

    fix: use config

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/google/go-cmp v0.7.0
 	github.com/lmittmann/tint v1.1.2
 	github.com/spf13/afero v1.14.0
-	github.com/suzuki-shunsuke/ghtkn-go-sdk v0.2.0-0
+	github.com/suzuki-shunsuke/ghtkn-go-sdk v0.2.0-1
 	github.com/suzuki-shunsuke/go-stdutil v0.0.0-20250829203246-323a2d220819
 	github.com/suzuki-shunsuke/slog-error v0.2.0
 	github.com/suzuki-shunsuke/urfave-cli-v3-util v0.0.8-0.20250830063646-87773e1bf444
--- pkg/cli/get/command.go
+++ pkg/cli/get/command.go
@@ -75,7 +75,9 @@ func (r *runner) Command() *cli.Command {
 // It configures the controller with flags and arguments, then executes the token retrieval.
 // Returns an error if configuration is invalid or token retrieval fails.
 func (r *runner) action(ctx context.Context, c *cli.Command) error { //nolint:cyclop
-	inputGet := &ghtkn.InputGet{}
+	inputGet := &ghtkn.InputGet{
+		UseConfig: true,
+	}
 	if m := flag.MinExpirationValue(c); m != "" {
 		d, err := time.ParseDuration(m)
 		if err != nil {
